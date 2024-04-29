28c84f9df2ff1034014acda06eb4946e8853cdc2
Guilherme <g.satilio@gmail.com>
2024-04-29 13:12:24 -0300

    Add project files.

---
+++ Orientacao_Objeto/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Pessoa
+//{
+public class Pessoa
+{
+    public string Nome;
+    public string Sobrenome;
+    public int Idade;
+    public string Telefone;
+    public string Celular;
+    public string Email;
+    public Pessoa() // método construtor
+    {
+
+    }
+    public string DefinirNome()
+    {
+        Console.WriteLine("Informe o nome desejado:");
+        return Console.ReadLine();
+    }
+    public string DefinirSobrenome()
+    {
+        Console.WriteLine("Informe o Sobrenome:");
+        return Console.ReadLine();
+    }
+    public int DefinirIdade()
+    {
+        Console.WriteLine("Informe a sua idade:");
+        return int.Parse(Console.ReadLine());
+    }
+    public void DefinirTelefone(string telefone)
+    {
+        this.Telefone = telefone;
+    }
+    public void DefinirCelular(string celular)
+    {
+        this.Celular = celular;
+    }
+    public void DefinirEmail(string email)
+    {
+        this.Email = email;
+    }
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Nome: {Nome}", this.Nome);
+        Console.Write($" {Sobrenome}", this.Sobrenome);
+        Console.WriteLine($"Idade: {this.Idade}");
+        Console.WriteLine($"Telefone: {this.Telefone}");
+        Console.WriteLine($"Celular: {this.Celular}");
+        Console.WriteLine($"Email: {this.Email}");
+        Console.WriteLine();
+    }
+}
---
+++ Orientacao_Objeto/Program.cs
@@ -0,0 +1,34 @@
+﻿string dados;
+//Classe | Objeto
+Pessoa objPessoa1 = new Pessoa();
+Pessoa objPessoa2 = new Pessoa();
+Pessoa objPessoa3 = new Pessoa();
+
+#region criarpessoa por funcao
+
+Pessoa criarPessoa()
+{
+    Pessoa objTemp = new Pessoa();
+    objTemp.Nome = objTemp.DefinirNome();
+    objTemp.Sobrenome = objTemp.DefinirSobrenome();
+    objTemp.Idade = objTemp.DefinirIdade();
+    Console.WriteLine("Informe o seu Telefone:");
+    dados = Console.ReadLine();
+    objTemp.DefinirTelefone(dados);
+    Console.WriteLine("Informe o seu Celular:");
+    dados = Console.ReadLine();
+    objTemp.DefinirCelular(dados);
+    Console.WriteLine("Informe o seu Email:");
+    dados = Console.ReadLine();
+    objTemp.DefinirEmail(dados);
+
+    return objTemp;
+}
+#endregion
+
+objPessoa1 = criarPessoa();
+objPessoa1.MostrarDados();
+objPessoa2 = criarPessoa();
+objPessoa2.MostrarDados();
+objPessoa3 = criarPessoa();
+objPessoa3.MostrarDados();
